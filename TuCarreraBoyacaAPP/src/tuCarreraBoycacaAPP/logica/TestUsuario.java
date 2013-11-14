51dd9750f724c761843f99765bd0a37b276473e0
RojasVictor <victoralfonso.rojas@uptc.edu.co>
2013-11-14 11:10:55 -0500

    se agrega la clase TestUsuario, pero no tiene conexion con la clase preguntaTest, segun lo indica el diagrama de clases

---
+++ TuCarreraBoyacaAPP/src/tuCarreraBoycacaAPP/logica/TestUsuario.java
@@ -0,0 +1,36 @@
+package tuCarreraBoycacaAPP.logica;
+
+import java.util.ArrayList;
+
+public class TestUsuario {
+
+	private int idTest;
+	private String nombreUsuario;
+	private ArrayList preguntaTest;
+	
+	public TestUsuario(){
+		
+	}
+
+	public String toString() {
+		return "TestUsuario [idTest=" + idTest + ", nombreUsuario="
+				+ nombreUsuario + "]";
+	}
+
+	public int getIdTest() {
+		return idTest;
+	}
+
+	public void setIdTest(int idTest) {
+		this.idTest = idTest;
+	}
+
+	public String getNombreUsuario() {
+		return nombreUsuario;
+	}
+
+	public void setNombreUsuario(String nombreUsuario) {
+		this.nombreUsuario = nombreUsuario;
+	}
+ 	
+}
