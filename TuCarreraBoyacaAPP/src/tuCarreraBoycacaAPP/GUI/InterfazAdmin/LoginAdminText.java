b0d41b6a17d46b0a6c5a1bdd28dc981944861b74
haroldPatino <harold.patino@uptc.edu.co>
2013-11-19 17:56:50 -0500

    Se genero la clase LoginAdminText, la cual captura el nick del administrador y su contraseña.

---
+++ TuCarreraBoyacaAPP/src/tuCarreraBoycacaAPP/GUI/InterfazAdmin/LoginAdminText.java
@@ -0,0 +1,70 @@
+/**
+ * 
+ */
+package tuCarreraBoycacaAPP.GUI.InterfazAdmin;
+
+import java.awt.GridLayout;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+/**
+ * @author harold_patino
+ *
+ */
+public class LoginAdminText extends JPanel{
+//Attributes-------------------------------
+	private JLabel lblUser;
+	private JLabel lblPassword;
+	private JTextField txtUser;
+	private JTextField txtPassword;
+	
+//Building---------------------------------
+	public LoginAdminText(){
+		this.setLayout(new GridLayout(2,2));
+	}
+//Methods----------------------------------
+	/**
+	 * Aca se inicializan todos los atributos
+	 */
+	public void inicializar(){
+		lblUser=new JLabel("Usuario:");
+		lblPassword=new JLabel("Contraseña");
+		txtUser=new JTextField();
+		txtPassword=new JTextField();
+	}
+	/**
+	 * Aca se agregan los elementos al panel
+	 */
+	public void agregar(){
+		this.add(lblUser);
+		this.add(txtUser);
+		this.add(txtPassword);
+	}
+	/**
+	 * @return the lblUser
+	 */
+	public JLabel getLblUser() {
+		return lblUser;
+	}
+	/**
+	 * @return the lblPassword
+	 */
+	public JLabel getLblPassword() {
+		return lblPassword;
+	}
+	/**
+	 * @return the txtUser
+	 */
+	public JTextField getTxtUser() {
+		return txtUser;
+	}
+	/**
+	 * @return the txtPassword
+	 */
+	public JTextField getTxtPassword() {
+		return txtPassword;
+	}
+	
+}
