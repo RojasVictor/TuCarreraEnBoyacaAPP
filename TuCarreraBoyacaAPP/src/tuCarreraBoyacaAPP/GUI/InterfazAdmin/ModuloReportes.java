77c4d06b011da14ffe859b3c2bd50a1c0c7d16a0
Judit11 <judit.cepeda@uptc.edu.co>
2013-11-30 15:20:02 -0500

    Se valido para que la ventana no se ampliara Se coloco un incono perosonalizado a la ventana Se valido para que se centrara la ventana del modulo

--- TuCarreraBoyacaAPP/src/tuCarreraBoyacaAPP/GUI/InterfazAdmin/ModuloReportes.java
+++ TuCarreraBoyacaAPP/src/tuCarreraBoyacaAPP/GUI/InterfazAdmin/ModuloReportes.java
@@ -3,8 +3,8 @@
  */
 package tuCarreraBoyacaAPP.GUI.InterfazAdmin;
 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.Toolkit;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -18,10 +18,9 @@ import java.awt.event.ActionListener;
 
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JComboBox;
-import javax.swing.JTextField;
+import javax.swing.JScrollBar;
 import javax.swing.ImageIcon;
 import javax.swing.SwingConstants;
-import javax.swing.JRadioButton;
 import javax.swing.JButton;
 import javax.swing.JTextArea;
 
@@ -29,7 +28,7 @@ import javax.swing.JTextArea;
  * @author Victor_Rojas
  *
  */
-public class ModuloReportes extends JFrame{
+public class ModuloReportes extends JFrame {
 
 	private JPanel contentPane;
 
@@ -49,10 +48,12 @@ public class ModuloReportes extends JFrame{
 		});
 	}
 
+
 	/**
 	 * Create the application.
 	 */
 	public ModuloReportes() {
+		setIconImage(Toolkit.getDefaultToolkit().getImage(this.getClass().getResource("Images/modulo_reportes.png")));
 		setTitle("MODULO REPORTES");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 486, 450);
@@ -61,28 +62,31 @@ public class ModuloReportes extends JFrame{
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
+		setResizable(false);
+		setLocationRelativeTo(null);
+		
 		JLabel label = new JLabel("");
-		label.setIcon(new ImageIcon(this.getClass().getResource("images/Logo_TCBAPP1.png")));
-		label.setBounds(10, 11, 157, 123);
+		label.setIcon(new ImageIcon(this.getClass().getResource("Images/LogoPrincipal_TCBAPP.png")));
+		label.setBounds(7, 11, 121, 95);
 		contentPane.add(label);
 
-		JLabel label_1 = new JLabel("TU CARRERA EN BOYACÁ APP");
-		label_1.setHorizontalAlignment(SwingConstants.CENTER);
-		label_1.setForeground(new Color(0, 0, 128));
-		label_1.setFont(new Font("Berlin Sans FB", Font.BOLD, 18));
-		label_1.setBounds(177, 23, 282, 40);
-		contentPane.add(label_1);
+		JLabel lbl_Titulo = new JLabel("TU CARRERA EN BOYACÁ APP");
+		lbl_Titulo.setHorizontalAlignment(SwingConstants.CENTER);
+		lbl_Titulo.setForeground(new Color(0, 0, 128));
+		lbl_Titulo.setFont(new Font("Berlin Sans FB", Font.BOLD, 20));
+		lbl_Titulo.setBounds(138, 23, 321, 40);
+		contentPane.add(lbl_Titulo);
 		
-		JLabel lblModuloDeReportes = new JLabel("Modulo de Reportes");
-		lblModuloDeReportes.setHorizontalAlignment(SwingConstants.CENTER);
-		lblModuloDeReportes.setForeground(new Color(0, 0, 128));
-		lblModuloDeReportes.setFont(new Font("Berlin Sans FB", Font.BOLD, 18));
-		lblModuloDeReportes.setBounds(177, 47, 295, 40);
-		contentPane.add(lblModuloDeReportes);
+		JLabel lbl_SubTitulo = new JLabel("Modulo de Reportes");
+		lbl_SubTitulo.setHorizontalAlignment(SwingConstants.CENTER);
+		lbl_SubTitulo.setForeground(new Color(0, 0, 128));
+		lbl_SubTitulo.setFont(new Font("Berlin Sans FB", Font.BOLD, 20));
+		lbl_SubTitulo.setBounds(160, 66, 247, 40);
+		contentPane.add(lbl_SubTitulo);
 		
 		JButton btnRegresar = new JButton("");
-		btnRegresar.setIcon(new ImageIcon(this.getClass().getResource("images/regresar.png")));
-		btnRegresar.setBounds(51, 349, 86, 39);
+		btnRegresar.setIcon(new ImageIcon(this.getClass().getResource("Images/btn_Regresar.png")));
+		btnRegresar.setBounds(51, 361, 114, 40);
 		btnRegresar.addActionListener(new ActionListener() {
 			
 			@Override
@@ -94,20 +98,25 @@ public class ModuloReportes extends JFrame{
 		contentPane.add(btnRegresar);
 		
 		JButton button = new JButton("");
-		button.setIcon(new ImageIcon(this.getClass().getResource("images/genera.png")));
-		button.setBounds(329, 356, 114, 32);
+		button.setIcon(new ImageIcon(this.getClass().getResource("Images/btn_Generar.png")));
+		button.setBounds(329, 369, 114, 32);
 		contentPane.add(button);
 		
-		JTextArea textArea = new JTextArea();
-		textArea.setBounds(51, 223, 392, 115);
-		contentPane.add(textArea);
+		JComboBox cmbBox_Modulos = new JComboBox();
+		cmbBox_Modulos.setModel(new DefaultComboBoxModel(new String[] {"Listar Programas Academicos", "Listar Instituciones de Educacion Superior", "Listar Preguntas"}));
+		cmbBox_Modulos.setFont(new Font("Berlin Sans FB", Font.PLAIN, 16));
+		cmbBox_Modulos.setToolTipText("");
+		cmbBox_Modulos.setBounds(51, 124, 392, 32);
+		contentPane.add(cmbBox_Modulos);
 		
-		JComboBox comboBox = new JComboBox();
-		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Listar Programas Academicos", "Listar Instituciones de Educacion Superior", "Listar Preguntas"}));
-		comboBox.setFont(new Font("Berlin Sans FB", Font.PLAIN, 16));
-		comboBox.setToolTipText("");
-		comboBox.setBounds(51, 145, 392, 59);
-		contentPane.add(comboBox);
+		JScrollBar scrollBar = new JScrollBar();
+		scrollBar.setBounds(426, 223, 17, 115);
+		contentPane.add(scrollBar);
+		
+		JTextArea txtArea_ListaReportes = new JTextArea();
+		txtArea_ListaReportes.setBounds(51, 223, 392, 115);
+		contentPane.add(txtArea_ListaReportes);
 		
 	}
 }
+
