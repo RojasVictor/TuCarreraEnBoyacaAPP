da5d21739fa4a1d1b93594237ec36f6e80c852b1
Judit11 <judit.cepeda@uptc.edu.co>
2013-12-03 13:46:08 -0500

    Se crea la clase donde se cargara el inicio de de la aplicacion

---
+++ TuCarreraBoyacaAPP/src/tuCarreraBoyacaAPP/GUI/InterfazUsuario/InicioAplicacion.java
@@ -0,0 +1,85 @@
+/**
+ * 
+ */
+package tuCarreraBoyacaAPP.GUI.InterfazUsuario;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JRootPane;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import java.awt.Color;
+import java.awt.SystemColor;
+
+/**
+ * @author JUDIT
+ *
+ */
+public class InicioAplicacion extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					InicioAplicacion frame = new InicioAplicacion();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public InicioAplicacion() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 295, 487);
+		contentPane = new JPanel();
+		contentPane.setBackground(SystemColor.inactiveCaptionBorder);
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		setResizable(false);
+		setLocationRelativeTo(null);
+		setUndecorated(true);
+		getRootPane().setWindowDecorationStyle(JRootPane.NONE);
+		
+		JLabel label = new JLabel("");
+		label.setIcon(new ImageIcon(InicioAplicacion.class.getResource("/tuCarreraBoyacaAPP/GUI/InterfazUsuario/Images/Logo_Principal.png")));
+		label.setBounds(39, 273, 232, 186);
+		contentPane.add(label);
+		
+		JLabel label_1 = new JLabel("");
+		label_1.setIcon(new ImageIcon(InicioAplicacion.class.getResource("/tuCarreraBoyacaAPP/GUI/InterfazUsuario/Images/imagen_estMujer.png")));
+		label_1.setBounds(29, 301, 54, 36);
+		contentPane.add(label_1);
+		
+		JLabel label_2 = new JLabel("");
+		label_2.setIcon(new ImageIcon(InicioAplicacion.class.getResource("/tuCarreraBoyacaAPP/GUI/InterfazUsuario/Images/imagen_estHombre.png")));
+		label_2.setBounds(217, 244, 54, 36);
+		contentPane.add(label_2);
+		
+		JLabel label_3 = new JLabel("");
+		label_3.setIcon(new ImageIcon(InicioAplicacion.class.getResource("/tuCarreraBoyacaAPP/GUI/InterfazUsuario/Images/imagen_universidad.png")));
+		label_3.setBounds(123, 253, 48, 56);
+		contentPane.add(label_3);
+		
+		JLabel label_4 = new JLabel("");
+		label_4.setBackground(new Color(224, 255, 255));
+		label_4.setIcon(new ImageIcon(InicioAplicacion.class.getResource("/tuCarreraBoyacaAPP/GUI/InterfazUsuario/Images/TituloInicio.png")));
+		label_4.setBounds(29, 6, 244, 227);
+		contentPane.add(label_4);
+
+	}
+}
