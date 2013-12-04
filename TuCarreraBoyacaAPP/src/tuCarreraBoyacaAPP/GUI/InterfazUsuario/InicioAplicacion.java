a9435d8fb527a4d349ae117d3d9fda9a24d8eab4
Judit11 <judit.cepeda@uptc.edu.co>
2013-12-03 22:19:53 -0500

    Se modifico para que mostrara la pantallaa del celular

--- TuCarreraBoyacaAPP/src/tuCarreraBoyacaAPP/GUI/InterfazUsuario/InicioAplicacion.java
+++ TuCarreraBoyacaAPP/src/tuCarreraBoyacaAPP/GUI/InterfazUsuario/InicioAplicacion.java
@@ -5,12 +5,18 @@ package tuCarreraBoyacaAPP.GUI.InterfazUsuario;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JProgressBar;
 import javax.swing.JRootPane;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.Border;
 import javax.swing.border.EmptyBorder;
+import javax.swing.border.SoftBevelBorder;
 import javax.swing.JLabel;
 import javax.swing.ImageIcon;
+
 import java.awt.Color;
 import java.awt.SystemColor;
 
@@ -43,10 +49,10 @@ public class InicioAplicacion extends JFrame {
 	 */
 	public InicioAplicacion() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 295, 487);
+		setBounds(100, 100, 305, 583);
 		contentPane = new JPanel();
-		contentPane.setBackground(SystemColor.inactiveCaptionBorder);
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setBackground(Color.WHITE);
+		contentPane.setBorder((Border) new SoftBevelBorder(BevelBorder.RAISED, null, null, null, null));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
@@ -55,31 +61,46 @@ public class InicioAplicacion extends JFrame {
 		setUndecorated(true);
 		getRootPane().setWindowDecorationStyle(JRootPane.NONE);
 		
-		JLabel label = new JLabel("");
-		label.setIcon(new ImageIcon(this.getClass().getResource("Images/Logo_Principal.png")));
-		label.setBounds(39, 273, 232, 186);
-		contentPane.add(label);
-		
 		JLabel label_1 = new JLabel("");
 		label_1.setIcon(new ImageIcon(this.getClass().getResource("Images/imagen_estMujer.png")));
-		label_1.setBounds(29, 301, 54, 36);
+		label_1.setBounds(174, 257, 54, 36);
 		contentPane.add(label_1);
 		
 		JLabel label_2 = new JLabel("");
 		label_2.setIcon(new ImageIcon(this.getClass().getResource("Images/imagen_estHombre.png")));
-		label_2.setBounds(217, 244, 54, 36);
+		label_2.setBounds(227, 231, 54, 36);
 		contentPane.add(label_2);
 		
 		JLabel label_3 = new JLabel("");
 		label_3.setIcon(new ImageIcon(this.getClass().getResource("Images/imagen_universidad.png")));
-		label_3.setBounds(123, 253, 48, 56);
+		label_3.setBounds(39, 266, 48, 56);
 		contentPane.add(label_3);
 		
 		JLabel label_4 = new JLabel("");
 		label_4.setBackground(new Color(224, 255, 255));
 		label_4.setIcon(new ImageIcon(this.getClass().getResource("Images/TituloInicio.png")));
-		label_4.setBounds(29, 6, 244, 227);
+		label_4.setBounds(42, 91, 216, 203);
 		contentPane.add(label_4);
-
+		
+		JProgressBar progressBar = new JProgressBar();
+		progressBar.setIndeterminate(true);
+		progressBar.setStringPainted(true);
+		progressBar.setForeground(new Color(178, 34, 34));
+		progressBar.setBackground(Color.WHITE);
+		progressBar.setBounds(41, 448, 230, 27);
+		contentPane.add(progressBar);
+		
+		JLabel label = new JLabel("");
+		label.setIcon(new ImageIcon(this.getClass().getResource("Images/Logo_Principal.png")));
+		label.setBounds(39, 256, 232, 186);
+		contentPane.add(label);
+		
+		
+		JLabel label_celular = new JLabel("");
+		label_celular.setIcon(new ImageIcon(this.getClass().getResource("Images/celularfondo.png")));
+		label_celular.setBounds(0, 0, 305, 583);
+		contentPane.add(label_celular);
 	}
+	
 }
+		
